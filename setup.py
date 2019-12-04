eb880be042b994b4d1ee2963c2b8d3931fd973d3
Chris Allen <allenc5@rpi.edu>
2019-12-04 11:29:47 -0500

    Added setup for pip install

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mockredis',
+    version='2.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
